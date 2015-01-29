547e601a122b86b689ad6c5f50c36a958afbc57d
abrosen <rosen@cs.gsu.edu>
2015-01-29 15:54:09 -0500

    Added the next assignment

---
+++ translate.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+standard = {'PHE': 'F', 'LEU': 'L', 'ILE':'I', 'MET':'M', 'MSE':'M', 'VAL':'V', 'SER':'S',
+            'PRO':'P','THR':'T','ALA':'A', 'TYR':'Y', 'HIS':'H', 'GLN':'Q',
+            'ASN':'N', 'LYS':'K', 'ASP':'D', 'GLU':'E', 'CYS':'C', 'TRP':'W', 
+            'ARG':'R', 'SER':'S','GLY':'G'}
+class Translator(object):
+    pass
